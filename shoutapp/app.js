951dd02b128c4e8e8ee7cb1f690fe7e432d016df
Greg Hoelzer <ghoelzer@redhat.com>
2016-07-01 16:27:44 -0500

    Simple "hello cassandra" example

--- shoutapp/app.js
+++ shoutapp/app.js
@@ -6,71 +6,22 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cassandra = require('cassandra-driver');
 
-var client = new cassandra.Client({contactPoints : ['172.30.107.103']});
-client.connect(function(err, result){
-    console.log('cassandra connected');
-});
+var client = new cassandra.Client({hosts: ['172.30.107.103:9042'], keyspace: 'test', username: 'cassandra', password: 'cassandra'});
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var user = require('./routes/user');
-var adduser = require('./routes/adduser');
-var edituser = require('./routes/edituser');
-var shouts = require('./routes/shouts');
-var addshout = require('./routes/addshout');
+client.execute('SELECT * FROM test_table', [],
 
-var app = express();
+  function(err, result) {
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    if (err) {
+		console.log('execute failed', err);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    } else {
+		for (var i = 0; i < result.rows.length; i++) {
+			console.log('id=' + result.rows[i].get('id') + ' test_value=' + result.rows[i].get('test_value'));
+		}
+		
+		process.exit(0);
+    }
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/user', user);
-app.use('/adduser', adduser);
-app.use('/edituser', edituser);
-app.use('/shouts', shouts);
-app.use('/addshout', addshout);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+  }
+);
